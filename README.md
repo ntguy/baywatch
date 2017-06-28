a6d6b8d62318378405e7c113ca0fca3649954b8d
Nicalai Guy <nicalaitguy@gmail.com>
2017-06-28 13:58:11 -0400

    Stores list items in array. Non-functional fav button

--- README.md
+++ README.md
@@ -2,8 +2,24 @@
 
 A catalog of Michael Bay's **extensive** filmography.
 
-```
-* Thing 1
-* Thing 2
-* Thing 3
-```
+## Day 3 Homework
+
+* Store the flicks in an array, along with of the DOM
+
+## Bonus Credit
+
+* Add a _fav_ button to each list item that changes the appearance of that item(for example, add a background color)
+
+### Mega Bonus Credit
+
+* Add a _remove_ button to each list item
+* Make that button actually work
+
+### Super Mega Bonus Credit
+
+*  Make sure that both of those buttons affect the array as well
+* Make _fav_ button toggle the _fav_ status
+
+### Super Mega Bonus Credit Hyper Fighting
+
+* Add buttons that move a flick up and down the list
---
+++ app.js
@@ -0,0 +1,45 @@
+const app = {
+  init(selectors) {
+    this.flicks = []
+    this.max = 0
+    this.list = document.querySelector(selectors.listSelector)
+
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener(
+        'submit', 
+        this.handleSubmit.bind(this)
+      )
+  },
+
+  renderListItem(flick) {
+    const item = document.createElement('li')
+    item.textContent = flick.name
+    const favbtn = document.createElement('button')
+    favbtn.textContent = 'Fav'
+    favbtn.style.backgroundColor = 'cornflowerBlue'
+    favbtn.style.marginLeft = '40px'
+    item.appendChild(favbtn)
+    return item
+  },
+
+  handleSubmit(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const flick = {
+      id: this.max + 1,
+      name: f.flickName.value,
+    }
+
+    const listItem = this.renderListItem(flick)
+    this.flicks[flick.id - 1] = flick.name
+    this.list.appendChild(listItem)
+
+    this.max ++
+  },
+}
+
+app.init({
+  formSelector: 'form#flick-form',
+  listSelector: '#flick-list',
+})
